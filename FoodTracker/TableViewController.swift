65fb1d0189c5b11b38e85fff855a385ea9cb587d
Hung Nguyen <hungnguyen@Hungs-MacBook-Air.local>
2017-07-19 18:07:28 +0700

    layout

--- FoodTracker/TableViewController.swift
+++ FoodTracker/TableViewController.swift
@@ -17,7 +17,7 @@ class TableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
